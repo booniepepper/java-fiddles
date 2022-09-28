841930d047b572c1748a5eeb8d1807e201dc2750
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-09-28 01:45:29 -0700

    use main graalvm distro

--- hello-graalvm/build.sh
+++ hello-graalvm/build.sh
@@ -11,6 +11,5 @@ cd "$builddir" || exit 1
 javac -d "$builddir" "$rootdir/src/main/java/so/dang/cool/fiddle/Greet.java"
 
 # https://www.graalvm.org/22.2/reference-manual/native-image/overview/Build-Overview/
-# mandrel is a downstream of graalvm community edition (it was packaged for Void)
-/usr/lib/jvm/mandrel11/bin/native-image --static --native-image-info so.dang.cool.fiddle.Greet greet
+/usr/lib/jvm/graalvm-ce-java17-22.2.0/bin/native-image --static --native-image-info so.dang.cool.fiddle.Greet greet
 
