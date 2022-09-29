662f685ab983593be30b63ca9f28eb4a60b9996e
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-09-28 21:59:16 -0700

    Use a jar

--- hello-graalvm/build.sh
+++ hello-graalvm/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+set -euo pipefail
 
 rootdir="$(dirname $0)"
 builddir="$rootdir/build"
@@ -8,8 +8,15 @@ builddir="$rootdir/build"
 mkdir -p "$builddir"
 cd "$builddir" || exit 1
 
-javac -d "$builddir" "$rootdir/src/main/java/so/dang/cool/fiddle/Greet.java"
+wget -nc https://repo1.maven.org/maven2/so/dang/cool/z/3.0.0/z-3.0.0.jar
+
+javac -cp z-3.0.0.jar -d "$builddir" "$rootdir/src/main/java/so/dang/cool/fiddle/Greet.java"
 
 # https://www.graalvm.org/22.2/reference-manual/native-image/overview/Build-Overview/
-/usr/lib/jvm/graalvm-ce-java17-22.2.0/bin/native-image --static --native-image-info so.dang.cool.fiddle.Greet greet
+/usr/lib/jvm/graalvm-ce-java17-22.2.0/bin/native-image \
+    -cp z-3.0.0.jar:"$builddir" \
+    --static \
+    --native-image-info \
+    --no-fallback \
+    so.dang.cool.fiddle.Greet greet
 
--- hello-graalvm/src/main/java/so/dang/cool/fiddle/Greet.java
+++ hello-graalvm/src/main/java/so/dang/cool/fiddle/Greet.java
@@ -1,11 +1,25 @@
 package so.dang.cool.fiddle;
 
+import so.dang.cool.z.Z;
+
 class Greet {
     public static void main(String[] args) {
-        String name = "world";
+        String name = firstOr(args, "world");
+       
+        Z.with(name)
+            .fuse(Greet::greetingFor)
+            .fuseConsumer(System.out::println)
+            .run();
+    }
+
+    public static String firstOr(String[] args, String otherwise) {
         if (args.length > 0) {
-            name = args[0];
+            return args[0];
         }
-        System.out.println("Hello " + name + "!");
+        return otherwise;
+    }
+
+    public static String greetingFor(String name) {
+        return "Hello " + name + "!";
     }
 }
