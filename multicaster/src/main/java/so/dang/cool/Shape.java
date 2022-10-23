627d6a2ddb8ed9ec757a248b37b43a693c0e8965
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-10-22 22:19:19 -0700

    Add note on generics

--- multicaster/src/main/java/so/dang/cool/Shape.java
+++ multicaster/src/main/java/so/dang/cool/Shape.java
@@ -7,6 +7,20 @@ public interface Shape {
 
     double perimeter();
 
+    /**
+     * It would be nice to be able to just have this:
+     * <pre>
+     * {@code
+     * default <T> Optional<T> to() {
+     *     if (this instanceof T t) return Optional.of(t);
+     *     return Optional.empty;
+     * }
+     * }
+     * </pre>
+     * However it's not possible. Generic types are only a compiler feature,
+     * not a feature of the virtual machine. They get erased during compilation
+     * and are not available during runtime (when the function is executed).
+     */
     default <T> Optional<T> to(Class<T> clazz) {
         return Optional.of(this).filter(clazz::isInstance).map(clazz::cast);
     }
